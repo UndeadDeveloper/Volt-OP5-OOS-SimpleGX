6e2312aa9b70ed50deb3ffd7b21c936f5ba4f85c
AndroidGX <subut3x@gmail.com>
2017-08-15 11:11:47 +0200

    import build tools

---
+++ simplegx_anykernel/anykernel.sh
@@ -0,0 +1,55 @@
+# AnyKernel2 ramdisk mod script
+# osm0sis @ xda-developers
+
+# *****************
+# ANYKERNEL SETUP *
+# *****************
+
+# begin properties
+properties() {
+do.devicecheck=1
+do.initd=0
+do.modules=1
+do.system_blobs=0
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=OnePlus 5
+device.name2=OnePlus5
+device.name3=oneplus5
+device.name4=cheeseburger
+device.name5=
+}
+# end properties
+
+# shell variables
+block=/dev/block/bootdevice/by-name/boot;
+add_seandroidenforce=0
+is_slot_device=0;
+
+## end setup
+
+# *******************
+# ANYKERNEL METHODS *
+# *******************
+
+# import patching functions/variables - see for reference
+. /tmp/anykernel/tools/ak2-core.sh;
+
+# permissions
+chmod -R 755 $ramdisk
+
+## AnyKernel install
+dump_boot;
+
+# begin ramdisk changes
+
+# init.rc
+backup_file init.qcom.rc;
+insert_line init.qcom.rc "init.simplegx.rc" after "import init.qcom.test.rc" "import init.simplegx.rc";
+
+# end ramdisk changes
+
+write_boot;
+
+## end install
+
